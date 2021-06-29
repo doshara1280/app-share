d78483a09e9eaa785f69465a8b7bd73266436246
doshara1280 <danyuan749xa@gmail.com>
2021-06-29 19:48:05 +0900

    アプリ編集機能を仮作成

---
+++ src/app/webapp-edit/webapp-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webapp-edit',
+  templateUrl: './webapp-edit.component.html',
+  styleUrls: ['./webapp-edit.component.css']
+})
+export class WebappEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
